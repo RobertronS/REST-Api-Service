7b25130ab70dc30d0f0552a6777b47ab7a2dac1e
Roberto Chavez <rchavez.intertec@gmail.com>
2021-12-01 19:37:12 +0300

    Rest API Service Task 1.1

--- src/main/java/com/roberto/pollster/QuestionController.java
+++ src/main/java/com/roberto/pollster/QuestionController.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+
 @RestController
 @RequestMapping(path = "/question", produces = "application/json")
 @CrossOrigin(origins = "*")
